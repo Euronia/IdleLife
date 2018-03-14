48821367df11033daa7231380b8d8542c8470dc1
Tmouton <tmouton@excilys.com>
2018-03-14 13:56:24 +0100

    [Feat][Prestige] Added basic prestige

--- src/app/model/prestige.model.ts
+++ src/app/model/prestige.model.ts
@@ -1,10 +1,25 @@
 
 import {Unlockable} from './unlockable.model';
+import {Price} from './price.model';
+import {Resource} from './resource.model';
 
 export class PrestigeCurrency extends Unlockable {
 
-  constructor(id: number, name: string, unlocked: boolean, quantity: number) {
+  public prices: Price[];
+  public description: string;
+
+  constructor(id: number, unlocked: boolean, name: string, quantity: number, price: Price[], description: string) {
     super(id, name, unlocked, quantity);
+    this.prices = price;
+    this.description = description;
   }
 
+  isBuyable(resources: Resource[]) {
+    this.prices.forEach(item => {
+      if (resources[item.unlockableId - 1].quantity < item.size) {
+        return false;
+      }
+    });
+    return true;
+  }
 }
